117c7590f8c89cae11ce1015aeaeb4e22da2a51e
Mihir TACC VM <ubuntu@mihirbendre-coe332-vm.js2local>
2024-05-03 16:27:23 +0000

    updates

--- src/worker.py
+++ src/worker.py
@@ -48,35 +48,50 @@ def all_values_for(param, data):
     return dict_of_values
 
 
+def top_5_values(data_dict):
+    # Sorting dictionary items based on values (occurrences) in descending order
+    sorted_data = sorted(data_dict.items(), key=lambda x: x[1], reverse=True)
+    
+    # Selecting the top 5 items
+    top_5 = sorted_data[:5]
+    
+    # Creating a new dictionary with the top 5 items
+    top_5_dict = dict(top_5)
+    
+    return top_5_dict
+
 def hist_plotter(param):
     response = requests.get(url)
     if response.status_code == 200:
         data = response.json()
         dict_of_values = all_values_for(param, data)
-
+    
+    top_5_dict = top_5_values(dict_of_values)
+    
     # Extracting keys (variables) and values (occurrences) from the dictionary
-    variables = list(dict_of_values.keys())
-    occurrences = list(dict_of_values.values())
+    variables = list(top_5_dict.keys())
+    occurrences = list(top_5_dict.values())
 
     # Plotting the histogram
     plt.bar(variables, occurrences, color='skyblue', edgecolor='black')
 
     # Adding labels and title
-    plt.xlabel(f"Values for {param}")
+    plt.xlabel(f"Top 5 values for {param}")
     plt.ylabel('Occurrences')
-    plt.title('Histogram for {param}')
+    plt.title(f"Histogram for {param}")
 
     # Rotating x-axis labels for better readability (optional)
     plt.xticks(rotation=45)
+    plt.xticks(fontsize=8)
 
     # Save the plot
     plt.savefig('/output_image.png')
 
 
+
 @q.worker
 def worker(jobid):
-    update_job_status(jobid, 'in progress')
-
+    update_job_status(jobid, 'in progress')    
     try:
         job_data = get_job_by_id(jobid)
     except ValueError:
@@ -86,18 +101,26 @@ def worker(jobid):
     job_type = job_data.get('job_type')
     
     if job_type == "histogram":
-        param = job_data.get('params')['param']
-        #param = "crime_type"
+        try:
+            param = job_data.get('params')['param']
+        except NameError:
+            logging.error("Parameter not found in job_data")
+        param = "crime_type"
         hist_plotter(param)
-
-        
-
-        with open('/output_image.png', 'rb') as f:
-            img = f.read()
-        res.hset(jobid, 'image', img)
     
+    with open('/output_image.png', 'rb') as f:
+        img = f.read()
+    res.hset(jobid, 'image', img)
     update_job_status(jobid, 'completed')
-    
+
+    '''
+    sample_plot()
+    with open('/output_image.png', 'rb') as f:
+        img = f.read()
+    res.hset(jobid, 'image', img)
+    update_job_status(jobid, 'completed')
+    '''
+
 worker()
 
 
