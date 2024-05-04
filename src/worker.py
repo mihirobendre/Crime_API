6435edccf62e673932860d18fd545ce138c4d38c
Avya Kalra <66273669+akalra501@users.noreply.github.com>
2024-05-03 22:07:10 -0500

    Update worker.py

--- src/worker.py
+++ src/worker.py
@@ -17,9 +17,7 @@ log_level = os.getenv("LOG_LEVEL", "INFO")
 
 logging.basicConfig(level=log_level)
 
-#_redis_ip = os.environ.get('REDIS_IP')
-_redis_ip = 'redis-db'
-_redis_port = '6379'
+REDIS_IP = os.environ.get('REDIS_IP')
 _list_of_jobs = []
 
 # These might not be needed, as we're importing them from jobs:
